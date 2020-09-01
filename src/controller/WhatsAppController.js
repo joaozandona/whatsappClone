b115f12f5d204ed248e725b2004ac23420970683
Joao Zandona <joaoricardozandona@hotmail.com>
2020-09-01 16:17:57 -0300

    Ele ja esta carregando a foto do usuario

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -7,37 +7,45 @@ import { User } from "../model/User.js";
 
 export class WhatsAppController {
   constructor() {
-
-    console.log('WhatsappController OK')
+    console.log("WhatsappController OK");
 
     this._firebase = new Firebase();
     this.initAuth();
     this.elementsPrototype();
     this.loadElements();
     this.initEvents();
-
   }
 
   initAuth() {
     this._firebase
       .initAuth()
       .then((response) => {
-        this._user = new User();
+        this._user = new User(response.user.email);
 
-        let userRef = User.findByEmail(response.user.email);
+        this._user.on("datachange", (data) => {
+          document.querySelector("title").innerHTML =
+            data.name + " -WhatsAppClone";
 
-        userRef.set({
-          name: response.user.displayName,
-          email: response.user.email,
-          photo: response.user.photoURL
-        }).then(()=>{
+            this.el.inputNmePanelEditProfile = data.name;
 
-          this.el.appContent.css({
-            display: 'flex'
-          });
+            if(data.photo){
+              let photo = this.el.imgPanelEditProfile;
+              photo.src = data.photo;
+              photo.show();
+              this.el.imgDefaultPanelEditProfile.hide();
+    
+              let photo2 = this.el.myPhoto.querySelector('img');
+              photo2.src = data.photo;
+              photo2.show();
+    
+            }
 
         });
-        
+
+        this.el.appContent.css({
+          display: 'flex'
+        });
+
       })
       .catch((err) => {
         console.error(err);

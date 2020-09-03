f74f1710408fc2fe8c71407e86c4926ba247deae
Joao Zandona <joaoricardozandona@hotmail.com>
2020-09-02 23:06:36 -0300

    Pronto agora ta reconhecendo quem mandou a mensagem

--- src/model/Message.js
+++ src/model/Message.js
@@ -6,6 +6,12 @@ export class Message extends Model {
   constructor() {
     super();
   }
+  get id() {
+    return this._data.id;
+  }
+  set id(value) {
+    return (this._data.id = value);
+  }
 
   get content() {
     return this._data.content;
@@ -34,9 +40,6 @@ export class Message extends Model {
   set status(value) {
     return this._data.status;
   }
-
-  get id(){ return this._data.id; }
-  set id(value){ return this._data.id = value; }
 
   getViewElement(me = true) {
     let div = document.createElement("div");
@@ -293,16 +296,20 @@ export class Message extends Model {
       default:
         div.innerHTML = `
     
-        <div class="font-style _3DFk6 tail" id="${this.id}">
+        <div class="font-style _3DFk6 tail" id="_${this.id}">
             <span class="tail-container"></span>
             <span class="tail-container highlight"></span>
             <div class="Tkt2p">
                 <div class="_3zb-j ZhF0n">
-                    <span dir="ltr" class="selectable-text invisible-space message-text">${this.content}</span>
+                    <span dir="ltr" class="selectable-text invisible-space message-text">${
+                      this.content
+                    }</span>
                 </div>
                 <div class="_2f-RV">
                     <div class="_1DZAH">
-                        <span class="msg-time">${Format.timeStampToTime(this.timeStamp)}</span>
+                        <span class="msg-time">${Format.timeStampToTime(
+                          this.timeStamp
+                        )}</span>
                     </div>
                 </div>
             </div>
@@ -322,8 +329,8 @@ export class Message extends Model {
       content,
       timeStamp: new Date(),
       status: "wait",
-      type: 'text',
-      
+      type,
+      from
     });
   }
 
